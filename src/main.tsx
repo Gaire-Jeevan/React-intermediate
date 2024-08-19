8431d94d1bbff2ce587cfca761f5d755434b3b0b
Jeevan <gairejeevan7@gmail.com>
2024-08-19 21:07:39 +0530

    Exercise Fetching Post Data.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReactQueryDevtools, ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+import {
+  ReactQueryDevtools,
+  ReactQueryDevtoolsPanel,
+} from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
-const queryCLient = new QueryClient();
+const queryCLient = new QueryClient(
+  // {
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, // 5min
+//       staleTime: 10 * 1000, // 10sec
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
