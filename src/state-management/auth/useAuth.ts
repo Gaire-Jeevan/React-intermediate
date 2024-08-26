6ea51a3787becdfeb9e7c95e4ba2be5d11506829
Jeevan <gairejeevan7@gmail.com>
2024-08-26 18:03:02 +0530

    Exercise: Organizing code.

--- src/state-management/auth/useAuth.ts
+++ src/state-management/auth/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext, useDebugValue } from "react";
-import AuthContext from "../contexts/authContext";
+import AuthContext from "./authContext";
 
 const useAuth = () => useContext(AuthContext);
 
