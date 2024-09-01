55832f386a04f0372d6589d76e3f221ee6afc65b
Jeevan <gairejeevan7@gmail.com>
2024-09-01 22:30:24 +0530

    Getting data about the current page.

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
+import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter([
     {path: '/', element: <HomePage />},
     {path: '/users', element: <UserListPage />},
-    {path: '/users/:id', element: <UserListPage />},
-    {path: '/contact', element: <UserDetailPage />},
+    {path: '/users/:id', element: <UserDetailPage />},
+    {path: '/contact', element: <ContactPage />},
 ])
 
 export default router;
