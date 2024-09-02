ab82a91cf9a3fb27f331013758361ae9d86d8a3f
Jeevan <gairejeevan7@gmail.com>
2024-09-02 17:53:00 +0530

    Handling Errors.

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,11 +1,14 @@
+import { error } from "console";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  throw new Error("unexpected error");
+
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
       <Link to="/users">Users</Link>
     </>
